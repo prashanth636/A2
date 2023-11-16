ef140a7beac1a0edac602c2ca914d137989beebc
prashanth636 <prashanth.varma636@gmail.com>
2023-11-16 02:41:28 -0500

    changes

--- a2.hpp
+++ a2.hpp
@@ -8,9 +8,24 @@
 
 #include <vector>
 #include <mpi.h>
+#include <iostream>
+#include <sstream>
 
 void isort(std::vector<short int>& Xi, MPI_Comm comm) {
+	
+	int number_of_process, rank_of_process; 
+    MPI_Comm_size(comm, &number_of_process);
+    MPI_Comm_rank(comm, &rank_of_process);
+	
+	std::ostringstream oss;
+    for (const auto& num : Xi) {
+        oss << num << " ";
+    }
 
+    std::string result = oss.str();
+
+    // Print the result
+    std::cout << "Rank: " << rank_of_process << " Resulting string: " << result << std::endl;
 } // isort
 
 #endif // A2_HPP
