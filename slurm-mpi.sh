d1c31594e5ae4c5b4d9f3e1b5c549117bc694446
prashanth636 <prashanth.varma636@gmail.com>
2023-11-17 15:16:47 -0500

    adding local sample

--- slurm-mpi.sh
+++ slurm-mpi.sh
@@ -14,20 +14,20 @@
 
 ####### further customizations
 ### name of your job
-#SBATCH --job-name="changeme"
+#SBATCH --job-name="abdTestMpi"
 
 ### files to store stdout and stderr (%j will be replaced by the job id)
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
 
 ### how many nodes to allocate for the job
-#SBATCH --nodes=4
+#SBATCH --nodes=3
 
 ### how many cores to use on each node
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=4
 
 ### max time the job will run
-#SBATCH --time=00:08:00
+#SBATCH --time=00:15:00
 
 # load module intel which provides Intel MPI
 # this code is Intel MPI specific
@@ -35,10 +35,7 @@ module load intel
 export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
 
 # run MPI code in parallel on all requested nodes/cores
-srun --mpi=pmi2 ./mpi-test
+srun --mpi=pmi2 ./a2 512
 
 echo "RUNNING ONLY HOSTNAME:"
 hostname
-
-echo "RUNNING SRUN HOSTNAME:"
-srun hostname
